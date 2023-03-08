0eef748d303a410cf7023c47264806e139cebc5d
hunterk <hunter_kaller@yahoo.com>
2023-03-07 19:56:27 -0600

    add input descriptors
    enables remapping in RetroArch

--- dirksimple_libretro.c
+++ dirksimple_libretro.c
@@ -542,6 +542,21 @@ static uint64_t get_current_inputbits(void)
     CHECK_JOYPAD_INPUT(SELECT, COINSLOT);
     CHECK_JOYPAD_INPUT(START, START);
     #undef CHECK_JOYPAD_INPUT
+    
+    struct retro_input_descriptor desc[] = {
+        { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_LEFT,   "Joystick Left" },
+        { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_UP,     "Joystick Up" },
+        { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_DOWN,   "Joystick Down" },
+        { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_RIGHT,  "Joystick Right" },
+        { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_A,      "Action 1" },
+        { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_X,      "Action 2" },
+        { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_SELECT, "Coin Slot" },
+        { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_START,  "Start" },
+	    
+        { 0 },
+    };
+
+    environ_cb(RETRO_ENVIRONMENT_SET_INPUT_DESCRIPTORS, desc);
 
     return joypad_inputbits | keyboard_inputbits;
 }
